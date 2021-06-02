35f9a15e98f9c5a47cf002851fc133f784a37094
arma7x <arma7x@live.com>
2021-06-03 03:36:37 +0800

    update

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require github.com/beego/beego/v2 v2.0.1
 
 require (
 	github.com/gorilla/websocket v1.4.2
+	github.com/robertkrimen/otto v0.0.0-20200922221731-ef014fd054ac
 	github.com/smartystreets/goconvey v1.6.4
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
 )
--- routers/router.go
+++ routers/router.go
@@ -4,12 +4,13 @@ import (
   "websms/controllers"
   "github.com/beego/beego/v2/server/web/context"
   beego "github.com/beego/beego/v2/server/web"
+  "github.com/robertkrimen/otto"
+  "net/http"
+  "log"
+  "io/ioutil"
+  "regexp"
+  "fmt"
 )
-
-type Payload struct {
-  Title string `json:"title"`
-  Body  string `json:"body"`
-}
 
 func init() {
   
@@ -22,6 +23,57 @@ func init() {
     ctx.ResponseWriter.Header().Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding, Accept-Language")
     ctx.ResponseWriter.Header().Add("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE, HEAD, OPTIONS")
     ctx.Output.JSON("{}", false, true)
+  })
+  
+  beego.Get("/decrypt", func(ctx *context.Context) {
+    player := ctx.Input.Query("player")
+    sig := ctx.Input.Query("sig")
+    if resp, err := http.Get(player); err != nil {
+      log.Println(err)
+    } else {
+      defer resp.Body.Close()
+      if resp.StatusCode == http.StatusOK {
+        bodyBytes, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+          ctx.Output.Body([]byte("Error"))
+        } else {
+          js := bodyBytes
+          re := `\n[^.]+\.split\(""\);.+`
+          child := regexp.MustCompile(re).Find(js)
+          if child == nil {
+            ctx.Output.Body([]byte("Error"))
+            return
+          }
+          re = `\w+`
+          childName := regexp.MustCompile(re).Find(child)
+          if childName == nil {
+            ctx.Output.Body([]byte("Error"))
+            return
+          }
+          re = `;(\w+)`
+          parentName := regexp.MustCompile(re).FindSubmatch(child)
+          if parentName == nil {
+            ctx.Output.Body([]byte("Error"))
+            return
+          }
+          re = fmt.Sprintf(`var %s=.+\n.+\n[^}]+}};`, parentName[1])
+          parent := regexp.MustCompile(re).Find(js)
+          if parent == nil {
+            ctx.Output.Body([]byte("Error"))
+            return
+          }
+          vm := otto.New()
+          if _, err := vm.Run(string(parent) + string(child)); err != nil {
+            ctx.Output.Body([]byte("Error"))
+          } else {
+            sig, err := vm.Call(string(childName), nil, sig)
+            if err == nil {
+              ctx.Output.Body([]byte(sig.String()))
+            }
+          }
+        }
+      }
+    }
   })
 
   beego.Router("/", &controllers.MainController{})
